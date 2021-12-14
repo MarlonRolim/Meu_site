91f178b101e1e9047308d0b53225181e888ce5d4
MarlonRolim <marlon@rolimsolutions.com>
2021-12-14 11:57:52 -0300

    Pagina Sobre

--- meu_site.py
+++ meu_site.py
@@ -13,6 +13,10 @@ def homepage():
 def contatos():
     return render_template("contatos.html")
 
+@app.route("/sobre")
+def contatos():
+    return render_template("sobre.html")
+
 @app.route("/usuarios/<nome_usuario>")
 def usuarios(nome_usuario):
     return render_template("usuarios.html", nome_usuario=nome_usuario)
